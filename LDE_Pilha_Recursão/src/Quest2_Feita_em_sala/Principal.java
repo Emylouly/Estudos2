3b830bc9858c95eff90895005dfd103a26a3362f
Emylouly <emilylouisemika@gmail.com>
2024-09-10 11:31:46 -0300

    e

---
+++ LDE_Pilha_Recursão/src/Quest2_Feita_em_sala/Principal.java
@@ -0,0 +1,12 @@
+package Quest2_Feita_em_sala;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Recursivo f = new Recursivo();
+        System.out.println(f.imprimirSequencia(30));
+
+    }
+
+}
---
+++ LDE_Pilha_Recursão/src/Quest2_Feita_em_sala/Recursivo.java
@@ -0,0 +1,43 @@
+package Quest2_Feita_em_sala;
+
+public class Recursivo {
+
+    public int calcularFibo(int a){
+
+        if(a<=1){
+
+            return a;
+
+        }
+
+        else{
+
+            return calcularFibo(a-1) + calcularFibo(a-2);
+
+        }
+
+    }
+
+    public String imprimirSequencia(int qtd){
+
+        String seq = "[";
+
+        for(int cont =0; cont<qtd; cont ++){
+
+            seq += calcularFibo(cont) + " ";
+
+            if(calcularFibo(cont)>100){
+
+                break;
+            }
+
+        }
+
+        seq += "]";
+
+        return seq;
+
+    }
+
+
+}
